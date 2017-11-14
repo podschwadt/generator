05abea54ea1d654e724915122494029803555ee7
Robert Podschwadt <robertpodschwadt@my.unt.edu>
2017-11-14 17:13:14 -0600

    added function to put mulitple images into one

--- tools.py
+++ tools.py
@@ -1,4 +1,6 @@
 import numpy as np
+from PIL import Image
+import sys
 
 class CircleList( object ):
 
@@ -22,3 +24,49 @@ class CircleList( object ):
 
     def variance( self ):
         return np.var( self.list )
+
+
+
+def image_grid( images, bg_color=( 0, 0, 0 ), distance=( 0, 0 ), debug=False ):
+    """
+    images must be numpy array of images in int [0;255]
+    """
+
+    #TODO automatically dertermine mode and reshaping
+    images = images.astype( 'uint8' )
+    if images.shape[ 3 ] == 1:
+        mode = 'L'
+    elif images.shape[ 3 ] == 3:
+        mode = 'RGB'
+    else:
+        raise Exception()
+    y_max = 0
+    x_max = 0
+    for i in images:
+        x_max = max( x_max, i.shape[ 0 ] )
+        y_max = max( y_max, i.shape[ 1 ] )
+    rows = np.ceil( np.sqrt( len( images ) ) ).astype( int )
+    new_size = ( rows * x_max + rows * distance[ 0 ] , rows * y_max + rows * distance[ 1 ] )
+    print( rows )
+    new_im = Image.new( 'RGB', new_size, color = bg_color )
+    row = 0
+    col = 0
+    count = 1
+    for i in images:
+        if row == rows:
+            col += 1
+            row = 0
+        #TODO automatically dertermine mode and reshaping
+        if i.shape[ 2 ] == 1:
+            i = i.reshape( i.shape[ : 2 ] )
+        old_im = Image.fromarray( i, mode=mode )
+        old_size = old_im.size
+        new_im.paste( old_im, ( row * x_max + row * distance[ 0 ], col * y_max + col * distance[ 1 ]  ) )
+        if debug:
+            sys.stdout.write('\rProcessing image %i/%i' % ( count, len( images ) ) )
+            sys.stdout.flush()
+            count += 1
+            print( ( row * x_max + row * distance[ 0 ], col * y_max + col * distance[ 1 ]  ) )
+        row += 1
+
+    return new_im
