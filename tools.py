91df1f16a7a6e128b2dd44e85fc33fde2ecda29b
Robert Podschwadt <robertpodschwadt@my.unt.edu>
2017-11-17 13:12:18 -0600

    added timer

--- tools.py
+++ tools.py
@@ -1,6 +1,9 @@
 import numpy as np
 from PIL import Image
 import sys
+import time
+
+
 
 class CircleList( object ):
 
@@ -17,13 +20,37 @@ class CircleList( object ):
         self.index = ( self.index + 1 ) % self.size
 
     def mean( self ):
-        return ( sum_it() * 1. ) / len( self.list )
+        return ( self.sum_it() * 1. ) / len( self.list )
 
     def sum_it( self ):
         return sum( self.list )
 
     def variance( self ):
         return np.var( self.list )
+
+
+class Timer( object ):
+
+    def __init__( self, steps, size=100, print_steps=10 ):
+        self.list = CircleList( 100 )
+        self.steps = steps
+        self.count = 0
+        self.print_steps = print_steps
+
+    def start_step( self ):
+        self.start = time.time()
+
+    def stop_step( self ):
+        self.list.add( time.time() - self.start )
+        self.count += 1
+
+    def __str__( self ):
+        return time.strftime( '%H:%M:%S', time.gmtime( ( self.steps - self.count ) * self.list.mean() ) )
+
+    def out( self ):
+        if self.count % self.print_steps == 0:
+            sys.stdout.write('\rremaining {} | {}/{} '.format( self, self.count, self.steps  ) )
+            sys.stdout.flush()
 
 
 
