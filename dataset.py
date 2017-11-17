dda954d674e62656a9b68ffd732a9dd903fa57e0
Robert Podschwadt <robertpodschwadt@my.unt.edu>
2017-11-17 17:59:18 -0600

    added channels first option

--- dataset.py
+++ dataset.py
@@ -3,12 +3,15 @@ import cPickle as pickle
 
 filename = 'dataset.data'
 
-def load( filename=filename ):
+def load( filename=filename, channels_first=False ):
     x = pickle.load( open( filename, 'rb' ) )
     np.random.shuffle( x )
     y = np.arange( x.shape[ 0 ] )
     y = np.ones_like( y )
     y = y.astype( 'float32' )
+
+    if channels_first:
+        x = np.rollaxis(x,3,1)
 
     x_test = x[  : x.shape[ 0 ] / 10 ]
     x_train = x[ ( x.shape[ 0 ] / 10 ) + 1 :  ]
